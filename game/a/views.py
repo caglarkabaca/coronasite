3f82956e2313fd7fd052e7f5d1094994cf4da81c
scp-999 <cacarekt@yandex.com>
2020-09-02 14:40:03 +0300

    34

--- game/a/views.py
+++ game/a/views.py
@@ -7,7 +7,39 @@ from django.http import HttpResponse
 
 def index(request):
 
+    import json
+    import random
+
+    from django.templatetags.static import static
+
+    with open(static('data.json')[1:], 'r') as f:
+
+        data = json.loads(f.read())
+
+    country_one = random.choice(data)
+    #data.remove(country_one)
+    country_two = random.choice(data)
+
+    score = '0'
+
+    if request.GET.get('score', False):
+
+        score = str(int(request.GET['score']) + 1)
+
+    count_one = int(country_one[2].replace(',',''))
+    count_two = int(country_two[2].replace(',',''))
+    
     template = loader.get_template('a/index.html')
-    context = {}
+    context = {
+        'u1_img' : country_one[3],
+        'u1_name' : country_one[1],
+        'u1_count' : country_one[2],
+        'u2_img' : country_two[3],
+        'u2_name' : country_two[1],
+        'u2_count' : country_two[2],
+        'score' : score,
+        'u1_count_int' : count_one,
+        'u2_count_int' : count_two,
+    }
 
     return HttpResponse(template.render(context, request))
